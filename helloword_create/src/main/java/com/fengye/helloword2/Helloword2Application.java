c41da39f7e5af4df4c9704ba27637ca6a14e88a9
devyf <hyfmailsave@163.com>
2021-03-16 12:34:10 +0800

    上传SpringBoot2项目搭建及HelloWorld入门

--- helloword_create/src/main/java/com/fengye/helloword2/Helloword2Application.java
+++ helloword_create/src/main/java/com/fengye/helloword2/Helloword2Application.java
@@ -3,7 +3,7 @@ package com.fengye.helloword2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  //标注这个类是一个springboot应用：启动类下的所有资源被导入
 public class Helloword2Application {
 
     public static void main(String[] args) {
--- helloword_create/src/main/java/com/fengye/helloword2/controller/HelloworldController.java
+++ helloword_create/src/main/java/com/fengye/helloword2/controller/HelloworldController.java
@@ -3,12 +3,14 @@ package com.fengye.helloword2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody
+@RestController  //相当于@Controller与@ResponseBody
 public class HelloworldController {
     @RequestMapping("hello")
-    @ResponseBody
     public String hello(){
-        return "hello SpringBoot Starter!";
+        return "hello SpringBoot Starter。。。!";
     }
 }
