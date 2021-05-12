83b0c97b5d637b4653167162be968ed724be21d6
huangyifeng <hyfmailsave@163.com>
2021-05-12 13:28:29 +0800

    提交mybatis逆向工程代码生成器

---
+++ springboot_mybatis/src/main/java/com/fengye/springboot_mybatis/entity/Product.java
@@ -0,0 +1,83 @@
+package com.fengye.springboot_mybatis.entity;
+
+public class Product {
+    private Long id;
+
+    private String productname;
+
+    private Long dirId;
+
+    private Double saleprice;
+
+    private String supplier;
+
+    private String brand;
+
+    private Double cutoff;
+
+    private Double costprice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname == null ? null : productname.trim();
+    }
+
+    public Long getDirId() {
+        return dirId;
+    }
+
+    public void setDirId(Long dirId) {
+        this.dirId = dirId;
+    }
+
+    public Double getSaleprice() {
+        return saleprice;
+    }
+
+    public void setSaleprice(Double saleprice) {
+        this.saleprice = saleprice;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier == null ? null : supplier.trim();
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand == null ? null : brand.trim();
+    }
+
+    public Double getCutoff() {
+        return cutoff;
+    }
+
+    public void setCutoff(Double cutoff) {
+        this.cutoff = cutoff;
+    }
+
+    public Double getCostprice() {
+        return costprice;
+    }
+
+    public void setCostprice(Double costprice) {
+        this.costprice = costprice;
+    }
+}
---
+++ springboot_mybatis/src/main/java/com/fengye/springboot_mybatis/mapper/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.fengye.springboot_mybatis.mapper;
+
+import com.fengye.springboot_mybatis.entity.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
